aaa04d86ffd92f96118b346b5e8ccbf5ed77d956
felipe.israel <felipe.israel@aluno.ifsp.edu.br>
2020-09-28 20:34:17 -0300

    Criação Backend com API no AdonisJS e Frontend com ReactJS

---
+++ backend/app/Controllers/Http/PostController.js
@@ -0,0 +1,89 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Post = use('App/Models/Post')
+
+/**
+ * Resourceful controller for interacting with posts
+ */
+class PostController {
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const posts = await Post.all()
+    return posts
+  }
+
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request}) {
+    const data = request.only(['content'])
+    const post = await Post.create(data)
+
+    return post
+  }
+
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params}) {
+    const post = await Post.find(params.id)
+
+    return post
+  }
+
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request}) {
+    const data = request.only(['content'])
+    const post = await Post.find(params.id)
+
+    post.merge(data)
+    await post.save()
+    return post
+  }
+
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params}) {
+    const post = await Post.find(params.id)
+
+    await post.delete()
+  }
+}
+
+module.exports = PostController
